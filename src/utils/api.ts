a65ce3a23f955d56f3563452e03637af3fca7d52
Kristina Gurachenkova <gurachenkova@yandex.ru>
2025-01-22 17:29:16 +0600

    fix type in api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,15 +4,6 @@ import axios from "axios";
 const API_URL = "https://jsonplaceholder.typicode.com/todos";
 
 export const fetchTasks = async (): Promise<Task[]> => {
-  const response = await axios.get<any>(API_URL);
-
-  const tasks: Task[] = response.data.map(
-    (item: { id: number; title: string; completed: boolean }) => ({
-      id: item.id,
-      title: item.title,
-      completed: item.completed,
-    })
-  );
-
-  return tasks;
+  const response = await axios.get<Task[]>(API_URL);
+  return response.data;
 };
